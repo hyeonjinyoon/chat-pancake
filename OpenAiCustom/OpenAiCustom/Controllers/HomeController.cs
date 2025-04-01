6e3e3d8fa786d4d9c706739b1fd13db5bb2562fb
hyeonjin <deok9@studiouvu.com>
2025-04-01 20:58:36 +0900

    copy 버튼 추가

--- OpenAiCustom/OpenAiCustom/Controllers/HomeController.cs
+++ OpenAiCustom/OpenAiCustom/Controllers/HomeController.cs
@@ -38,10 +38,15 @@ public class HomeController : Controller
     public async Task<JsonResult> ReceiveText([FromBody] ContentTextDataModel model)
     {
         if (string.IsNullOrEmpty(model.Data))
-            return Json(new { error = "No data provided" });
+            return Json(new
+            {
+                error = "No data provided"
+            });
 
         var question = model.Data;
-        
+
+        Console.WriteLine($"Received question: {question}");
+
 //         var context = """
 //                       If you think that splitting the question into multiple parts and re-asking it might yield better results,
 //                       then please separate the question into several questions and answer them.
@@ -66,19 +71,21 @@ public class HomeController : Controller
 //                                    """;
 
         var timeBegin = DateTime.Now;
-        
+
         var answer = await OpenAiManager.GetChat(question);
         
+        var answerId = Guid.NewGuid().ToString();
+
         var timeComplete = DateTime.Now;
 
         // var answerWithMarkdown = Markdown.ToHtml(answer).Replace("<p>", "").Replace("</p>", "");
-        
+
         var answerHtml = $"""
                           <div style="color: #707070;">
                           {(timeComplete - timeBegin).TotalSeconds:N0}초 동안 조리
                           </div>
                           <div
-                              id="answer"
+                              id="answer-{answerId}"
                               style="margin-top: 8px;
                               margin-right: auto; 
                               width: fit-content; 
@@ -88,15 +95,20 @@ public class HomeController : Controller
                               max-width: 600px;
                               white-space: pre-wrap;
                               overflow: auto;
-                              align-items: center;"><pre>{answer}</pre></div>
+                              align-items: center;"></div>
+                              
+                              <div style='width: 20px; height: 20px; margin-right: auto; color: #919191; cursor: pointer;' onclick=copyToClipboard('answer-{answerId}')>
+                              <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-copy' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z'/></svg>
+                              </div>
                           """;
 
         var data = new
         {
             html = answerHtml,
             content = answer,
+            answerId = $"answer-{answerId}",
         };
-        
+
         return Json(data);
     }
 }
